1dc9e42b09fa24e285d54b1263f555d5286b20d7
Shoooray <noahszhang>
2024-10-15 16:42:27 -0400

    double push because of goofy ah errors

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -56,17 +56,18 @@ public final class Constants
 
   public class Intake {
         /* IDs */
-        public static final int intakeMotorID = 3;
+        public static final int intakeMotorID = 20;
         /* CANBus */
         public static final String intakeMotorCanBus = "rio";
         /* Motor Speed Values */
-        public static final double intakingSpeed = 0.50;
-        public static final double ejectingSpeed = -0.50;
-        public static final double stoppingSpeed = 0.00;
+       // public static final double intakingSpeed = 0.50;
+       // public static final double ejectingSpeed = -0.50;
+       // public static final double stoppingSpeed = 0.00;
         /* Motor Config Values */
-        public static final double peakForwardVoltage = 12.0;
-        public static final double peakReverseVoltage = -12.0;
-        public static final InvertedValue motorOutputInverted = InvertedValue.Clockwise_Positive;
-        public static final NeutralModeValue motorNeutralValue = NeutralModeValue.Brake;
+       // public static final double peakForwardVoltage = 12.0;
+        //public static final double peakReverseVoltage = -12.0;
+        //public static final InvertedValue motorOutputInverted = InvertedValue.Clockwise_Positive;
+        //public static final NeutralModeValue motorNeutralValue = NeutralModeValue.Brake;
     }  
+
 }
