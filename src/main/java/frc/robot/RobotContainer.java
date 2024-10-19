a4626414fbf871a433a3a53f90956902ec764da6
Yoshi <Jesuslv2060@gmail.com>
2024-10-19 11:32:38 -0400

    autonomous

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -139,11 +139,11 @@ public class RobotContainer
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
    * @return the command to run in autonomous
-   */
+   */ 
   public Command getAutonomousCommand()
   {
     // An example command will be run in autonomous
-    return drivebase.getAutonomousCommand("B.O");
+    return drivebase.getAutonomousCommand("donee is the goat");
   }
 
   public void setDriveMode()
