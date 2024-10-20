d677dde939d7672b14c2d5836e0cc474733c5ec7
Shoooray <noahszhang@gmail.com>
2024-10-20 18:58:27 -0400

    fixed merge issues

--- src/main/java/frc/robot/commands/IntakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommand.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.IntakeSubsystem;
 
 public class IntakeCommand extends Command{
+    
     private final IntakeSubsystem intake;
 
     public IntakeCommand(IntakeSubsystem subsystem) {
